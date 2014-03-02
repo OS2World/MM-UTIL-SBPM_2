0398a710e75f5bf03790429a96ce4797415a677b
Martin Iturbide <martin@os2world.com>
2014-03-02 09:00:50 -0500

    0.99b

---
+++ sbpm2.h
@@ -0,0 +1,68 @@
+#define  ID_resource         1
+
+#define  IDM_SETTINGS        17
+#define  IDM_ADVANCED        18
+#define  IDM_ABOUT           19
+#define  SBBASEPORT          23
+#define  UseBlasterExtension 114
+
+#define  CDSLIDERLEFT        500
+#define  CDSLIDERRIGHT       501
+#define  MAINSLIDERLEFT      502
+#define  MAINSLIDERRIGHT     503
+#define  ENABLEBLASTER       26
+
+#define TrebleSliderLeft            504
+#define TrebleSliderRight           505
+#define BassSliderLeft              506
+#define BassSliderRight             507
+#define GainSliderLeft              508
+#define GainSliderRight             509
+
+#define ResetBlaster                115
+#define VolumeSbPro                 100
+#define VolumeSB16                  700
+#define MusicSettings               300
+#define LockLeftRightSlider         305
+
+#define LineInLeft                  510
+#define LineInRight                 511
+#define VocLeft                     512
+#define VocRight                    513
+#define FmLeft                      514
+#define FmRight                     515
+#define Microphone                  516
+#define IDM_MUSIC                   55
+
+#define ForceSettings               301
+#define hInputGainLeft              522
+#define hInputGainRight             523
+#define hOutMicrophone              521
+#define hControlRightCdAudio        206
+#define HLeftCdAudio                207
+#define HcontrolLineRight           208
+#define HLeftControlLineAudio       209
+#define hSpeaker                    210
+#define hRightFmAudio               409
+#define hLeftInputFm                410
+#define AdvancedSBpro               200
+
+#define hMicSelect                  2205
+#define hCdAudioSelect              2206
+#define HLineInSelect               2207
+
+#define hLowPass                    214
+#define hHighPass                   215
+#define hFilterOn                   217
+#define AdvancedSb16                400
+#define hAutomaticGain              401
+#define hMicrophone                 402
+#define hRightCdAudio               404
+#define hLeftCdAudioControl         405
+#define hRightLineInControl         406
+#define hLeftLineInControl          408
+#define hRightCdAudioControl        411
+#define hMicrophoneControl          412
+#define hStartMinimized             302
+#define hLowPas                     211
+#define FastOverride                303
